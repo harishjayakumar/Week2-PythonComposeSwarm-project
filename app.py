09fa9511961eb747b04449a0ec7fd82d2c1c3a43
Harish Jayakumar <jharish24@yahoo.com>
2015-08-12 16:28:31 -0700

    changed redis host variable
    added line
    redis_host=os.getenv('REDIS_HOST')
    redis = Redis(host=redis_host, port=6379)

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask
 from redis import Redis
 import os
 app = Flask(__name__)
-redis = Redis(host='redis', port=6379)
+redis_host=os.getenv('REDIS_HOST')
+redis = Redis(host=redis_host, port=6379)
 
 @app.route('/')
 def hello():
