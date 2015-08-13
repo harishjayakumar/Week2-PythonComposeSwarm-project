7146602a20899b260a7b205e7e3459f2c4da4c01
Harish Jayakumar <jharish24@yahoo.com>
2015-08-13 15:35:12 -0700

    Adding txt from ec2 land

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ redis = Redis(host=redis_host, port=6379)
 @app.route('/')
 def hello():
     redis.incr('hits')
-    return 'Hello World again.I have been seen %s times.' % redis.get('hits') 
+    return 'Hello World again.From Ec2 land.I have been seen %s times.' % redis.get('hits') 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
