e4905bb134b032fb0bc99b361a8d1a4d8f120270
harishjayakumar <harishjayakumar@Harishs-MacBook-Pro.local>
2015-09-05 18:27:04 -0700

    Changed message echoed on browser

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ redis = Redis(host=redis_host, port=6379)
 @app.route('/')
 def hello():
     redis.incr('hits')
-    return 'Hello World again. A demo on a Friday?.I have been seen %s times.' % redis.get('hits') 
+    return 'Hello World again. Team demo on a Friday?.I have been seen %s times.' % redis.get('hits') 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
