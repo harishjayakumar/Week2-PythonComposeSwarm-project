66bcfb7a2dcabbad0d2ee223792b568f93645dc9
Matt Bentley <mbentley@mbentley.net>
2016-09-02 10:57:55 -0400

    Updated compose files; modified app to use networks
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

--- app.py
+++ app.py
@@ -2,13 +2,12 @@ from flask import Flask
 from redis import Redis
 import os
 app = Flask(__name__)
-redis_host=os.getenv('REDIS_HOST')
-redis = Redis(host=redis_host, port=6379)
+redis = Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello():
     redis.incr('hits')
-    return 'Docker Demo - Real time demo  .I have been seen %s times.' % redis.get('hits') 
+    return 'Docker Demo - Real time demo. I have been seen %s times.' % redis.get('hits')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
