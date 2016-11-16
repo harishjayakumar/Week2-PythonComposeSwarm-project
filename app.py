c16b1c4cc032b62cdf7d8590e11630d59a909e04
Harish Jayakumar <jharish24@yahoo.com>
2016-11-16 13:40:48 -0800

    Removed Let's

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ redis = Redis(host='redis', port=6379)
 @app.route('/')
 def hello():
     redis.incr('hits')
-    return 'Docker Demo - Azure Demo Live -Let's do this times 3. I have been seen %s times.' % redis.get('hits')
+    return 'Docker Demo - Azure Demo Live - Done this 3 times. I have been seen %s times.' % redis.get('hits')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
