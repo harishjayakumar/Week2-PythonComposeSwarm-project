35f3a73e93b62cad71b5e5bbac6e115c0d0cd28b
Harish Jayakumar <jharish24@yahoo.com>
2017-03-22 06:58:17 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ redis = Redis(host='redis', port=6379)
 @app.route('/')
 def hello():
     redis.incr('hits')
-    return 'Docker Demo - Levis Demo Test . I have been seen %s times.' % redis.get('hits')
+    return 'Docker Demo - Levis Demo Test 2. I have been seen %s times.' % redis.get('hits')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
