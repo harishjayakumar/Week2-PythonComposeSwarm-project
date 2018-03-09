6b5d6e9c9990ac7e9cc3b00a05f374baa4f5f0dd
Harish Jayakumar <jharish24@yahoo.com>
2018-03-08 17:38:48 -0800

    comment

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ redis = Redis(host='redis', port=6379)
 @app.route('/')
 def hello():
     redis.incr('hits')
-    return 'Docker Demo - Happy Holidays  Demo Test 4. I have been seen %s times.' % redis.get('hits')
+    return 'Docker Demo - Happy Holidays  Demo Test 5. I have been seen %s times.' % redis.get('hits')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
